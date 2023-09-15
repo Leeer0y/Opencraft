e1da34999ee35f408d255e0b4ca7c526c5a7d37f
Leeer0y <51188745+Leeer0y@users.noreply.github.com>
2023-09-15 16:16:29 +1000

    Spinning cube!
    Created cube and made it spin! The cube is index drawn. now to add textures!

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,10 @@ int main()
         return -1;
     }
 
+    // Configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+
+
     Shader ourShader(RESOURCES_PATH "baseCube.vert", RESOURCES_PATH "test.frag");
 
     //glUseProgram(shaderProgram);
@@ -63,16 +67,55 @@ int main()
          0.0f,  0.5f, 0.0f  // top   
     }; 
 
-    unsigned int VBO, VAO;
+    // cube verticies
+    glm::vec3 cubeVerticies[] = {
+        // front
+        glm::vec3(-0.5, -0.5, 0.5), // 1
+        glm::vec3(-0.5, 0.5, 0.5), // 2
+        glm::vec3(0.5, 0.5, 0.5), // 3
+        glm::vec3(0.5, -0.5, 0.5), // 4
+        // back
+        glm::vec3(-0.5, -0.5, -0.5), // 5
+        glm::vec3(-0.5, 0.5, -0.5), // 6
+        glm::vec3(0.5, 0.5, -0.5), // 7
+        glm::vec3(0.5, -0.5, -0.5) // 8
+    };
+
+    unsigned int indicies[] = {
+        // front face
+        0, 1, 2,
+        0, 2, 3,
+        // back face
+        4, 5, 6,
+        6, 7, 6,
+        // left face
+        4, 5, 1,
+        0, 4, 1,
+        // right face
+        3, 7, 2,
+        6, 2, 7,
+        // top face
+        1, 6, 2,
+        1, 5, 6,
+        // bottom face
+        0, 4, 7,
+        3, 0, 7
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerticies), cubeVerticies, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -89,19 +132,19 @@ int main()
 
         // Rendering here
         glClearColor(0.1f, 0.1f, 0.4f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ourShader.use();
 
         // Create transformations
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); // aspect ratio will be messed up upon resize, SCR_WIDTH and SCR_HEIGHT are constant.
 
         ourShader.setMat4("model", model);
         ourShader.setMat4("view", view);
@@ -109,7 +152,9 @@ int main()
 
         // render container
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 72, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
